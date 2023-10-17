073edadba3ef59cb5737fee467e6d30cbfe69638
Elkhan <example@gmail.com>
2023-10-17 23:38:19 +0400

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+console.log('arr' + " - object"); 
+console.log(4 + "5"); 
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(17%6);
+
+console.log(2*4 === 8);
+
+const isChecked = false,
+      isClose = false;
+      isOpen = true;
+      
+      
+
+console.log(isChecked && isClose && isOpen);
